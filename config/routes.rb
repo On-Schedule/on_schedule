45a8fdfb203eb249aa4870588fe44b863ad9a955
Doug Welchons <doug.welchons@gmail.com>
2023-03-22 11:21:52 -0600

    add devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'dashboard#show'
-   match '*path', to: 'dashboard#show', via: :get, constraints: ->(request) { !request.path.start_with?('/rails') && !request.path.start_with?('/api') }
+  match '*path', to: 'dashboard#show', via: :get, constraints: ->(request) { !request.path.start_with?('/rails') && !request.path.start_with?('/api') }
 end
