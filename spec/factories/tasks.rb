b653e7f752bd573388a4b33d8980c8156d7ecafa
Doug Welchons <doug.welchons@gmail.com>
2023-03-25 10:21:35 -0600

    Add rubocop and rubocop fixes

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
     project
-    name {FFaker::AnimalUS.common_name}
+    name { FFaker::AnimalUS.common_name }
     start_date { Date.tomorrow }
     end_date { start_date + 2.weeks }
   end
