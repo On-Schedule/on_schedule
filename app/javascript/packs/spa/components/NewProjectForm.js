dd7222a7f6e81463e81186fdeb1f2234c6d37bfe
Doug Welchons <doug.welchons@gmail.com>
2023-03-26 22:26:28 -0600

    add new project form

--- app/javascript/packs/spa/components/NewProjectForm.js
+++ app/javascript/packs/spa/components/NewProjectForm.js
@@ -1,5 +1,175 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function NewProjectForm() {
-  return <h4> You are here </h4>
+const fiveEights = {
+  days: ["monday", "tuesday", "wednesday", "thursday", "friday"],
+  hours: 8
+}
+
+const fourTensMT = {
+  days: ["monday", "tuesday", "wednesday", "thursday"],
+  hours: 10
+}
+
+const fourTensTF = {
+  days: ["tuesday", "wednesday", "thursday", "friday"],
+  hours: 10
+}
+
+const defaultProject = {
+  name: "",
+  start_date: "",
+  end_date: "",
+  schedule: fiveEights
+}
+
+export default function NewProjectForm({project=defaultProject}) {
+  const [details, setDetails] = useState(project)
+  const [schedule, setSchedule] = useState(details.schedule)
+  const [custom, setCustom] = useState(true)
+
+  const updateDetail = (field) => (e) => {
+    setDetails((details) => ({...details, [field]: _.get(e, 'target.value', e)}))
+  }
+
+  console.log(details.name);
+  useEffect(() => {
+    setDetails({...details, schedule: schedule})
+  }, [schedule])
+
+  const updateSchedule = () => (e) => {
+    setCustom(false);
+    switch(e.target.value) {
+      case "5x8 (M-F)":
+        setSchedule(fiveEights);
+        break;
+      case "4x10 (M-Th)":
+        setSchedule(fourTensMT);
+        break;
+      case "4x10 (Tu-F)":
+        setSchedule(fourTensTF);
+        break;
+      default:
+        setCustom(true);
+    }
+  }
+
+  const setDays = () => (e) => {
+    const newDay = e.target.value
+    if(_.includes(schedule.days, newDay)) {
+      const days = _.filter(schedule.days, (day) => (day != newDay))
+      setSchedule({...schedule, days: days})
+    } else {
+      setSchedule({...schedule, days: [...schedule.days, newDay]})
+    }
+  }
+
+  const setHours = () => (e) => {
+    setSchedule((schedule) => ({...schedule, hours: e.target.value}))
+  }
+
+  const saveProject = async () => {
+    if(!valid()) {
+      return
+    } else {
+      // add POST request to backend, and redirect to project show page
+    }
+  }
+  const valid = () => {
+    // need to add validations for
+    // - name present
+    // - start_date & end_date present
+    // - start_date is before end_date
+    // - hours are between 1 & 24
+    // - at least on day selected
+    return true
+  }
+
+  return <div className="card text-white bg-primary mb-3 primary-card-wrapper">
+    <h4 className="card-header">Create A New Project</h4>
+    <form>
+      <div className="card-body">
+      <div className="form_group">
+        <label className="form-label mt-4">Project Name</label>
+        <input
+          className="form-control"
+          name="name"
+          placeholder="Project Name"
+          value={details?.name}
+          onChange={updateDetail('name')}
+        />
+      </div>
+      <div className="form_group list-inline-item">
+        <label className="form-label mt-4">Start Date</label>
+        <input
+          className="form-control"
+          name="startDate"
+          type='date'
+          lable="Start Date"
+          value={details?.start_date}
+          onChange={updateDetail('start_date')}
+        />
+      </div>
+      <div className="form_group list-inline-item">
+        <label className="form-label mt-4">End Date</label>
+        <input
+          className="form-control"
+          name="endDate"
+          type='date'
+          lable="End Date"
+          value={details?.end_date}
+          onChange={updateDetail('end_date')}
+        />
+      </div>
+      <div className="form-group" onChange={updateSchedule()}> <label className="form-label mt-4 ">Work Week</label><br/>
+        {_.map(["5x8 (M-F)", "4x10 (M-Th)", "4x10 (Tu-F)", "Custom schedule"], (preSet) => (
+          <div key={preSet} className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              value={preSet}
+              type="radio"
+              name="workWeek"
+              defaultChecked={preSet === "Custom schedule"}
+            />
+            <label className="form-label">{preSet}</label>
+          </div>
+        ))}
+      </div>
+      </div>
+      { custom ? (
+        <div className="form-group card-body bg-dark">
+          {_.map(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'], (day) => (
+            <div key={day} className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value={day}
+                name="day"
+                checked={_.includes(details.schedule.days, day)}
+                disabled={!custom}
+                onChange={setDays()}
+              />
+              <label className="form-label">{_.upperFirst(day)}</label>
+            </div>
+          ))}
+          <div className=" list-inline-item">
+            <label className="form-label mt-4">Workday Hours</label>
+            <input
+              className="form-control"
+              type="number"
+              name="hours"
+              min={1}
+              max={24}
+              value={details.schedule.hours}
+              disabled={!custom}
+              onChange={setHours()}
+            />
+          </div>
+        </div>
+      ) : ("")}
+      {/* Add team members (with check for read only) */}
+      <div className="card-body text-center">
+        <button className="btn btn-primary" onClick={saveProject()}>Save</button>
+      </div>
+    </form>
+  </div>
 }
