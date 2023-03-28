1c1d190961aa4fa0ea2394cacd27be350bad40c3
Doug Welchons <doug.welchons@gmail.com>
2023-03-27 22:48:17 -0600

    fix minor bugs

--- app/javascript/packs/spa/components/dashboard/ProjectsCard.js
+++ app/javascript/packs/spa/components/dashboard/ProjectsCard.js
@@ -9,7 +9,7 @@ export default function ProjectsCard() {
     <div className="card border-primary mb-3">
       <div className="card-header navbar">
         <h4>Projects</h4>
-        <Link to="/new_project" className="btn btn-info btn-sm d-flex">+ Add New Project</ Link>
+        <Link to="/projects/new" className="btn btn-info btn-sm d-flex">+ Add New Project</ Link>
       </div>
       <div className="card-body">
         {projects ? (
