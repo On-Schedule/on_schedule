32bffa5ddbf7a5e445704edafaa0081adc5a561e
Doug Welchons <doug.welchons@gmail.com>
2024-03-08 16:39:48 -0700

    all validation messages, and checks

--- app/javascript/packs/spa/components/NewProjectForm.js
+++ app/javascript/packs/spa/components/NewProjectForm.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
 import { newProject } from 'actions/projects'
 
 const fiveEights = {
@@ -73,7 +72,7 @@ export default function NewProjectForm({project=defaultProject}) {
   }
 
   const saveProject = () => async () => {
-    if(!valid()) {
+    if(!isValid()) {
       return;
     }
 
@@ -81,84 +80,100 @@ export default function NewProjectForm({project=defaultProject}) {
     navigate(`/projects/${project.id}`)
   }
 
-  const valid = () => {
-    // - start_date & end_date present
-    if (!details.start_date || !details.end_date) {
-      return false
-    }
+  const isValid = () => {
+    return (
+      nameIsValid() &&
+      datesExsist() &&
+      datesAreValid() &&
+      daysAreValid() &&
+      hoursAreValid()
+    )
+  }
 
-    // - start_date is before end_date
-    if (details.end_date < details.start_date) {
-      return false
+  const nameIsValid = () => {
+    if (details.name.length >= 1) {
+      return true
     }
+    return false
+  }
+  const datesExsist = () => {
+    if (details.start_date && details.end_date) {
+      return true
+    }
+    return false
+  }
 
-    // - hours are between 1 & 24
-    if (details.schedule.hours < 1 || details.schedule.hours > 24) {
-      return false
+  const datesAreValid = () => {
+    if (details.start_date < details.end_date) {
+      return true
     }
+    return false
+  }
 
-    // - at least one day selected
-    if (details.schedule.days.length < 1) {
-      return false
+  const daysAreValid = () => {
+    if (details.schedule.days.length >= 1) {
+      return true
     }
+    return false
+  }
 
-    // - name present
-    if(details.name.length < 1) {
-      return false
+  const hoursAreValid = () => {
+    if (details.schedule.hours >= 1 && details.schedule.hours <= 24) {
+      return true
     }
-    return true
+    return false
   }
 
   return <div className="card text-white bg-primary mb-3 primary-card-wrapper">
     <h4 className="card-header">Create A New Project</h4>
     <form>
       <div className="card-body">
-      <div className="form_group">
-        <label className="form-label mt-4">Project Name</label>
-        <input
-          className="form-control"
-          name="name"
-          placeholder="Project Name"
-          value={details?.name}
-          onChange={updateDetail('name')}
-        />
-      </div>
-      <div className="form_group list-inline-item">
-        <label className="form-label mt-4">Start Date</label>
-        <input
-          className="form-control"
-          name="startDate"
-          type='date'
-          lable="Start Date"
-          value={details?.start_date}
-          onChange={updateDetail('start_date')}
-        />
-      </div>
-      <div className="form_group list-inline-item">
-        <label className="form-label mt-4">End Date</label>
-        <input
-          className="form-control"
-          name="endDate"
-          type='date'
-          lable="End Date"
-          value={details?.end_date}
-          onChange={updateDetail('end_date')}
-        />
-      </div>
-      <div className="form-group" onChange={updateSchedule()}> <label className="form-label mt-4 ">Work Week</label><br/>
-        {_.map(["5x8 (M-F)", "4x10 (M-Th)", "4x10 (Tu-F)", "Custom schedule"], (preSet) => (
-          <div key={preSet} className="form-check form-check-inline">
-            <input
-              className="form-check-input"
-              value={preSet}
-              type="radio"
-              name="workWeek"
-              defaultChecked={preSet === "5x8 (M-F)"}
-            />
-            <label className="form-label">{preSet}</label>
-          </div>
-        ))}
-      </div>
+        <div className="form_group">
+          <label className="form-label mt-4">Project Name</label>
+          <input
+            className="form-control"
+            name="name"
+            placeholder="Project Name"
+            value={details?.name}
+            onChange={updateDetail('name')}
+          />
+        </div>
+        <div className="form_group list-inline-item">
+          <label className="form-label mt-4">Start Date</label>
+          <input
+            className="form-control"
+            name="startDate"
+            type='date'
+            lable="Start Date"
+            value={details?.start_date}
+            onChange={updateDetail('start_date')}
+          />
+        </div>
+        <div className="form_group list-inline-item">
+          <label className="form-label mt-4">End Date</label>
+          <input
+            className="form-control"
+            name="endDate"
+            type='date'
+            lable="End Date"
+            value={details?.end_date}
+            onChange={updateDetail('end_date')}
+          />
+        </div>
+        <div className="form-group" onChange={updateSchedule()}> <label className="form-label mt-4 ">Work Week</label><br/>
+          {_.map(["5x8 (M-F)", "4x10 (M-Th)", "4x10 (Tu-F)", "Custom schedule"], (preSet) => (
+            <div key={preSet} className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                value={preSet}
+                type="radio"
+                name="workWeek"
+                defaultChecked={preSet === "5x8 (M-F)"}
+              />
+              <label className="form-label">{preSet}</label>
+            </div>
+          ))}
+        </div>
       </div>
       { custom ? (
         <div className="form-group card-body bg-dark">
@@ -192,8 +207,17 @@ export default function NewProjectForm({project=defaultProject}) {
         </div>
       ) : ("")}
       {/* Add team members (with check for read only) */}
+      {!isValid() ? <div className="card-body">
+        <div className="card-body bg-dark">
+          {nameIsValid() ? "" : <div className="text-danger">- Name is required</div>}
+          {datesExsist() ? "" : <div className="text-danger">- Start and end dates are required</div>}
+          {datesAreValid() ? "" : <div className="text-danger">- Start date must be before end date</div>}
+          {daysAreValid() ? "" : <div className="text-danger">- At least one day must be selected</div>}
+          {hoursAreValid() ? "" : <div className="text-danger">- Hours must be between 1 and 24</div>}
+        </div>
+      </div> : ""}
       <div className="card-body text-center">
-        <button type="button" className="btn btn-primary" onClick={saveProject()} disabled={!valid()}>Save</button>
+        <button type="button" className="btn btn-primary" onClick={saveProject()} disabled={!isValid()}>Save</button>
       </div>
     </form>
   </div>
