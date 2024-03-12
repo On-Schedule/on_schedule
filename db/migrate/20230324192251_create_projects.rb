b27f1dff502c99e9dbb764a9390c55262164bb7f
Doug Welchons <doug.welchons@gmail.com>
2024-03-12 16:25:47 -0600

    change to uuid

--- db/migrate/20230324192251_create_projects.rb
+++ db/migrate/20230324192251_create_projects.rb
@@ -1,8 +1,8 @@
 class CreateProjects < ActiveRecord::Migration[6.1]
   def change
-    create_table :projects do |t|
+    create_table :projects, id: :uuid do |t|
       t.string :name
-      t.belongs_to :company, null: false, foreign_key: true
+      t.belongs_to :company, null: false, foreign_key: true, type: :uuid
       t.date :start_date
       t.date :end_date
       t.jsonb :schedule
--- db/migrate/20230324192549_add_details_to_users.rb
+++ db/migrate/20230324192549_add_details_to_users.rb
@@ -1,6 +1,6 @@
 class AddDetailsToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_reference :users, :company, null: false, foreign_key: true
+    add_reference :users, :company, null: false, foreign_key: true, type: :uuid
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
