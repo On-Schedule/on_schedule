b5323522215ab0523bf1b937a4d1098d30fc9a50
Doug Welchons <doug.welchons@gmail.com>
2024-03-23 16:07:39 -0600

    remove import for useEffect

--- app/javascript/packs/spa/components/projectPage/NewTaskForm.js
+++ app/javascript/packs/spa/components/projectPage/NewTaskForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from 'react-redux';
 import { useSelector } from "react-redux";
 import { newTask } from 'actions/tasks'
