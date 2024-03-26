0d2c9aad65a388b60f52085abd1bd6cdb15f9869
Doug Welchons <doug.welchons@gmail.com>
2024-03-25 23:27:05 -0600

    add scale and refactor date bar generation

--- app/javascript/packs/spa/components/projectPage/DateBar.js
+++ app/javascript/packs/spa/components/projectPage/DateBar.js
@@ -2,57 +2,77 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { DateTime } from "luxon"
 
-export default function DateBar({gridTemp}) {
+export default function DateBar({gridTemp, scale}) {
   const project = useSelector((state) => state.project)
-  const start_date = new Date(DateTime.fromISO(project?.start_date))
-  const end_date = new Date(DateTime.fromISO(project?.end_date))
-  const [projectDays, setProjectDays] = useState([])
-  const [projectMonths, setProjectMonths] = useState([])
+  const startDate = DateTime.fromISO(project?.start_date)
+  const endDate = DateTime.fromISO(project?.end_date)
+  const [dateBarInfo, setDateBarInfo] = useState([])
 
   const calculateDays = (date) => {
     const endOfMonth = date.endOf('month').startOf('day')
-    const endDate = DateTime.fromJSDate(end_date)
-    const endOfMonthDiff = endOfMonth.diff(date, 'days').as("days");
-    const endDateDiff = endDate.diff(date, 'days').as("days");
-    return endDateDiff > endOfMonthDiff ? endOfMonthDiff + 1 : endDateDiff + 1
+    const endOfMonthDiff = endOfMonth.diff(date, 'days').as("days") +1;
+    const endDateDiff = endDate.diff(date, 'days').as("days") +1;
+    return endOfMonthDiff < endDateDiff ? endOfMonthDiff : endDateDiff
   }
 
   useEffect(() => {
-    buildSchedule();
+    createDateBar();
   }, [project]);
 
-  const buildSchedule = () => {
-    var projectDays = [];
-    var projectMonths = []
-    var currentMonth = ""
-    var offset = 0
-
-    for (var d = start_date; d <= end_date; d.setDate(d.getDate() + 1)) {
-      projectDays.push((new Date(d)).getDate());
-      var month = DateTime.fromJSDate(d).toFormat('LLLL')
-
-      if (month != currentMonth) {
-        var days = calculateDays(DateTime.fromJSDate(d))
-        currentMonth = month
-        var monthObject = {month: month, days: days, offset: offset}
-        if (days <= 3) {
-          monthObject = {...monthObject, month: DateTime.fromJSDate(d).toFormat('LLL')}
-        }
-        projectMonths.push(monthObject)
-        offset += days
+  const monthValues = (date, days) => {
+    const longMonth = date.toFormat('LLLL')
+    const shortMonth = date.toFormat('LLL')
+    const noMonth = ""
+    var day = days <= 3 ? shortMonth : longMonth
+    var month = longMonth
+      if (days < 10){
+        month = noMonth
+      } else if (days < 20) {
+        month = shortMonth
       }
-    }
-    setProjectDays(projectDays)
-    setProjectMonths(projectMonths)
+      return {"month": month, "day": day, "year": shortMonth}
   }
 
+  const createDateBar = () => {
+    var offset = 0
+    var months = []
+
+    for (var d = startDate; d <= endDate; d = d.plus({months: 1}).startOf("month")) {
+      var days = calculateDays(d)
+      var startDay = d.get("day")
+      var monthObject = {month: monthValues(d, days), days: days, offset: offset, startRange: startDay, endRange: startDay + days}
+      months.push(monthObject)
+      offset += days
+    }
+    setDateBarInfo(months)
+  }
 
   return <div className="grid sticky-top" style={{...gridTemp, backgroundColor: "var(--bs-card-bg)"}}>
-    {_.map(projectMonths, (month, index) => (
-      <div key={index} className="date-bar" style={{gridColumn: `${month["offset"] + 2} / span ${month["days"]}`}} >{month["month"]}</div>
+    {_.map(dateBarInfo, (month, indexM) => (
+      <div key={indexM} className="date-bar" style={{gridColumn: `${month.offset + 2} / span ${month.days}`, gridRow: "1"}} ><span className=" sticky-left sticky-right" style={{"--stick-l": "250px"}}>{month.month[scale]}</span></div>
     ))}
-    {_.map(projectDays, (day, index) => (
-      <div key={index} className="date-bar" style={{gridColumn: `${index + 2}`}} >{day}</div>
-    ))}
+    {scale === "day" ? _.map(dateBarInfo, (month, indexM) => (
+      _.map(_.range(month.startRange, month.endRange), (day, indexD) => (
+        <div key={`${indexM}-${indexD}`} className="date-bar" style={{gridColumn: `${month.offset + indexD + 2}`, gridRow: "2"}} >{day}</div>
+    )))) : "" }
   </div>
 }
+
+// option 1 (better but throws and error about unique keys)
+// {_.map(dateBarInfo, (month, indexM) => (
+//   <>
+//     <div key={indexM} className="date-bar" style={{gridColumn: `${month.offset + 2} / span ${month.days}`, gridRow: "1"}} >{month.month[scale]}</div>
+//     { scale === "day" ? _.map(_.range(month.startRange, month.endRange), (day, indexD) => (
+//       <div key={`${indexM}-${indexD}`} className="date-bar" style={{gridColumn: `${month.offset + indexD + 2}`, gridRow: "2"}} >{day}</div>
+//     )) : ""}
+//   </>
+// ))}
+
+// option 2 (less performant but no unique keys error)
+// {_.map(dateBarInfo, (month, indexM) => (
+//   <div key={indexM} className="date-bar" style={{gridColumn: `${month.offset + 2} / span ${month.days}`, gridRow: "1"}} >{month.month[scale]}</div>
+// ))}
+// {scale === "day" ? _.map(dateBarInfo, (month, indexM) => (
+//   _.map(_.range(month.startRange, month.endRange), (day, indexD) => (
+//     <div key={`${indexM}-${indexD}`} className="date-bar" style={{gridColumn: `${month.offset + indexD + 2}`, gridRow: "2"}} >{day}</div>
+// )))) : "" }
--- app/javascript/packs/spa/components/projectPage/TaskList.js
+++ app/javascript/packs/spa/components/projectPage/TaskList.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from 'react-redux';
 import DateBar from "./DateBar";
+
+const projectScale = {
+  "day": 40,
+  "Week": 10,
+  "month": 5,
+  "year": .75
+}
+// const projectScaleEM = {
+//   "day": "2.5em",
+//   "Week": ".625em",
+//   "month": ".3175em",
+//   "Year": ".0635em"
+// }
 
 export default function TaskList() {
   const tasks = useSelector((state) => state.tasks)
   const duration = useSelector((state) => state.project?.duration)
   const columnOffset = 1
-  const cellSize = 50
+  const [cellSize, setCellSize] = useState(projectScale["day"])
+  const [scale, setScale] = useState("day")
   const gridTemp = {
     gridTemplateColumns: `250px repeat(${duration}, ${cellSize}px)`,
     width: `${(duration * cellSize) + 250}px`
+  }
+
+  const changeScale = (newScale) => {
+    setCellSize(projectScale[newScale])
+    setScale(newScale)
   }
 
   const taskIndexes = (task) => {
@@ -30,7 +49,7 @@ export default function TaskList() {
 
   return <div className="card-body">
     <div className="schedule-body">
-      <DateBar gridTemp={gridTemp} />
+      <DateBar gridTemp={gridTemp} scale={scale} />
       <div style={{marginTop: "10px"}}>
         {tasks?.length > 0 ? (
           _.map(_.sortBy(tasks, "start_date"), (task, index) => (
@@ -43,6 +62,11 @@ export default function TaskList() {
           <h4 className="card-title">No tasks for this project</h4>
         )}
       </div>
+    </div>
+    <div className="card-body text-center">
+      <button className="btn btn-sm btn-outline-info" onClick={() => (changeScale("day"))} > Day </button>
+      <button className="btn btn-sm btn-outline-info" onClick={() => (changeScale("month"))} > Month </button>
+      {/* <button className="btn btn-sm btn-outline-info" onClick={() => (changeScale("year"))} > Year </button> */}
     </div>
   </div>
 }
