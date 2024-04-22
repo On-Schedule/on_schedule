665054cd0abcbb0cbfed8fffc7db3bdc183f1c6c
Doug Welchons <doug.welchons@gmail.com>
2024-04-22 13:48:04 -0600

    update task factory

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :task do
     project
-    name { FFaker::AnimalUS.common_name }
-    start_date { (project.start_date...project.end_date).to_a.sample() }
-    end_date { (start_date...project.end_date).to_a.sample() }
-    hours { (4...50).to_a.sample() * ((end_date - start_date).to_i + 1) }
+    name { "#{FFaker::AnimalUS.common_name} enclosure" }
+    start_date { (project.start_date...project.end_date).to_a.sample }
+    end_date { (start_date...project.end_date).to_a.sample }
+    hours { (4...50).to_a.sample * ((end_date - start_date).to_i + 1) }
   end
 end
