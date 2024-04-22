e85bf99a20513ccc5606ec25938d1fc92dbfe0bd
Doug Welchons <doug.welchons@gmail.com>
2024-04-22 15:52:51 -0600

    update gemfile and test configs

--- Gemfile
+++ Gemfile
@@ -65,8 +65,6 @@ group :test do
   gem 'webmock'
   gem 'selenium-webdriver', '>= 4.0.0.rc1'
   gem 'capybara-selenium'
-  # Easy installation and use of web drivers to run system tests with browsers
-  gem 'webdrivers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,15 +3,15 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions': {
-      args: ['headless', 'enable-features=NetworkService,NetworkServiceInProcess']
-    }
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: ['headless', 'no-sandbox', 'disable-gpu', 'disable-dev-shm-usage']
   )
 
-  Capybara::Selenium::Driver.new app,
+  Capybara::Selenium::Driver.new(
+    app,
     browser: :chrome,
-    desired_capabilities: capabilities
+    options:
+  )
 end
 
 Capybara.default_driver = :headless_chrome
