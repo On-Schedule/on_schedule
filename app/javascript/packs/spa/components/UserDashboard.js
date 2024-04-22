7107201da22f93b2915d7aec14bf691d7caeb56a
Doug Welchons <doug.welchons@gmail.com>
2024-04-21 21:49:34 -0600

    add place holder for overview card

--- app/javascript/packs/spa/components/UserDashboard.js
+++ app/javascript/packs/spa/components/UserDashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProjectsCard from './dashboard/ProjectsCard';
 import CurrentTasksCard from './dashboard/CurrentTasksCard';
+import OverviewCard from './dashboard/OverviewCard';
 
 export default function UserDashboard() {
   return <div className='dashboard-wrapper flex-wrap'>
     <ProjectsCard />
     <CurrentTasksCard />
+    <OverviewCard />
   </div>
 }
