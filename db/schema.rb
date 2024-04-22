652047b1580181c3e581b48f64923a6d4e2b3f35
Doug Welchons <doug.welchons@gmail.com>
2024-04-22 14:42:07 -0600

    update

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_03_24_194405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension 'pgcrypto'
 
   create_table "companies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
