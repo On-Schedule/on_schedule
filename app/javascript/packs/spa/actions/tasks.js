2994e453586c574e2458513d97f1c77c9fa7349f
Doug Welchons <doug.welchons@gmail.com>
2024-05-16 16:45:01 -0600

    add updateTask action to front end

--- app/javascript/packs/spa/actions/tasks.js
+++ app/javascript/packs/spa/actions/tasks.js
@@ -6,6 +6,14 @@ export function newTask(details, projectID) {
   }
 }
 
+export function updateTask(details, projectID, taskID) {
+  return async (dispatch, _getState, api) => {
+    const {data:task} = await api.patch(`projects/${projectID}/tasks/${taskID}`, {"task": details});
+    dispatch({type: "task/received", task});
+    return task;
+  }
+}
+
 export function getTasks(projectID) {
   return async (dispatch, _getState, api) => {
     const {data:tasks} = await api.get(`projects/${projectID}/tasks`);
