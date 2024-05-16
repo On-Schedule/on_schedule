edf9c4b82df9e91137c357db213fc07177a060ee
Doug Welchons <doug.welchons@gmail.com>
2024-05-16 17:33:26 -0600

    add new task form to task item

--- app/javascript/packs/spa/components/projectPage/task.js
+++ app/javascript/packs/spa/components/projectPage/task.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { DateTime } from "luxon";
+import NewTaskForm from "./NewTaskForm";
 
 export default function Task({task, index, mainGridTemplate, gridTemp, taskCardWidth}) {
   const [openAccordion, setOpenAccordion] = useState(false)
   const openTaskItemWidth = 500
   const start_date = DateTime.fromISO(task.start_date).toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
   const end_date = DateTime.fromISO(task.end_date).toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
+  const [edit, setEdit] = useState(false)
   const itemWidths = {
     "--open-item-width": `${openTaskItemWidth}px`,
     "--closed-item-width": `${taskCardWidth}px`
@@ -36,12 +38,12 @@ export default function Task({task, index, mainGridTemplate, gridTemp, taskCardW
   return <div className="grid" style={mainGridTemplate}>
     <div className="sticky-left">
       <div className="task-items base-slide-out" style={{width: openAccordion ? `${openTaskItemWidth}px` : `${taskCardWidth}px`}}>
-          <FontAwesomeIcon
-            icon={faChevronUp}
-            className={openAccordion ? "flip" : "un-flip"}
-            style={{padding: "0px 5px", color: "var(--bs-gray-700)"}}
-            onClick={() => {setOpenAccordion(!openAccordion)}}
-          />
+        <FontAwesomeIcon
+          icon={faChevronUp}
+          className={openAccordion ? "flip" : "un-flip"}
+          style={{padding: "0px 5px", color: "var(--bs-gray-700)"}}
+          onClick={() => {setOpenAccordion(!openAccordion); setEdit(false)}}
+        />
         {task.name}
       </div>
       <div className={`base-slide-out ${openAccordion ? "open-task-item" : "close-task-item"}`} style={itemWidths}>
@@ -53,6 +55,7 @@ export default function Task({task, index, mainGridTemplate, gridTemp, taskCardW
               Average Daily Manpower: {task.daily_manpower}<br/>
               Cost Code: {task.cost_code} <br/>
               Hours: {task.hours} <br/>
+              <button className="btn btn-sm btn-dark" onClick={()=>{setEdit(true)}}> Edit </button>
               {/* also... Responsibility, in process? percent complete (based on number of days)? */}
             </div>
           </div>
@@ -62,5 +65,12 @@ export default function Task({task, index, mainGridTemplate, gridTemp, taskCardW
     <div className="grid" style={gridTemp}>
       <div className="schedule-bar" style={{...taskIndexes(task), ...color(task)}} />
     </div>
+    {edit &&
+    <div className="card" style={{position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)", zIndex: "3000", maxWidth: "50em"}}>
+      <div className="card-body" >
+        <NewTaskForm task={task} setEdit={setEdit}/>
+      </div>
+    </div>
+    }
   </div>
 }
