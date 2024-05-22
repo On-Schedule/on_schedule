d45fd8eebd48f5c579604bae17b3e63d6f256022
Doug Welchons <doug.welchons@gmail.com>
2024-05-21 22:18:09 -0600

    fix indentation

--- app/javascript/packs/spa/components/CompanyAdminPage.js
+++ app/javascript/packs/spa/components/CompanyAdminPage.js
@@ -3,7 +3,7 @@ import ProjectTemplates from "./companyAdminPage/ProjectTemplates";
 
 export default function CompanyAdminPage() {
   return <div>
-      <div className="card" style={{margin: "10px"}}>
+    <div className="card" style={{margin: "10px"}}>
       <div className="card-header">Company Admin Page</div>
       <div className="card-body">
         place holder<br/>
