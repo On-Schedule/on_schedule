64c6cfea96e773b4af15c1735f4d60aa8d1f9d82
Doug Welchons <doug.welchons@gmail.com>
2024-05-21 22:03:03 -0600

    add faX to modal

--- app/javascript/packs/spa/components/common/Modal.js
+++ app/javascript/packs/spa/components/common/Modal.js
@@ -1,5 +1,8 @@
 import React, { forwardRef } from "react";
 import { func, element, shape, string } from 'prop-types';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from '@fortawesome/free-solid-svg-icons'
+
 
 const Modal = forwardRef(function Modal(props, ref) {
   const {
@@ -12,7 +15,10 @@ const Modal = forwardRef(function Modal(props, ref) {
 
   return <div ref={ref} className={`${className} card modal`} style={style}>
     <div className="card-header" style={{display: "flex"}}>
-      {headerText} <span style={{marginLeft: "auto"}} onClick={closeModal}> X </span>
+      {headerText}
+      <span style={{marginLeft: "auto"}} onClick={closeModal}>
+        <FontAwesomeIcon icon={faX} />
+      </span>
     </div>
     <div className="card-body" >
       {children}
