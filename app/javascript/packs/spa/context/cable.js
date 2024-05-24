5212c775bf166bf7f4e741259d1556c058bf7f02
Doug Welchons <doug.welchons@gmail.com>
2024-05-24 16:07:15 -0600

    fix indintation

--- app/javascript/packs/spa/context/cable.js
+++ app/javascript/packs/spa/context/cable.js
@@ -4,12 +4,12 @@ import ActionCable from "actioncable";
 const CableContext = React.createContext();
 
 function CableProvider({ children }) {
-    const actionCableUrl = process.env.NODE_ENV === 'production' ? 'wss://on-schedule-d880dc83bd42.herokuapp.com/cable' : 'ws://localhost:3000/cable'
+  const actionCableUrl = process.env.NODE_ENV === 'production' ? 'wss://on-schedule-d880dc83bd42.herokuapp.com/cable' : 'ws://localhost:3000/cable'
 
-    const CableApp = {}
-    CableApp.cable = ActionCable.createConsumer(actionCableUrl)
+  const CableApp = {}
+  CableApp.cable = ActionCable.createConsumer(actionCableUrl)
 
-    return <CableContext.Provider value={CableApp}>{children}</CableContext.Provider>;
+  return <CableContext.Provider value={CableApp}>{children}</CableContext.Provider>;
 }
 
 export { CableContext, CableProvider };
