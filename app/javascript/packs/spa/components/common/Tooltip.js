86480a08c2e48224ec6740c41bdd560f96ade199
Doug Welchons <doug.welchons@gmail.com>
2024-05-27 19:07:51 -0600

    update tooltip styles

--- app/javascript/packs/spa/components/common/Tooltip.js
+++ app/javascript/packs/spa/components/common/Tooltip.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 export default function Tooltip({children}) {
-  return <div style={{position: "absolute", maxWidth: "5000px", zIndex: "2500"}}>
+  return <div className="tooltip-wrapper">
     <div className="tooltip-arrow-l" />
     <div className="tooltip-arrow-r" />
-    <div className="" style={{backgroundColor: "var(--bs-primary)", marginTop: "-6px", borderRadius: "5px", padding: "0 10px", display: "block"}}>
+    <div className="tooltip-body">
       {children}
     </div>
   </div>
