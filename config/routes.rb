717e6e578f5fd20630663c7c3f18e731ef1ced06
Doug Welchons <doug.welchons@gmail.com>
2024-07-23 16:11:50 -0600

    add todo destroy route and action

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
       resources :projects, only: [:create, :show] do
         resources :tasks, only: [:index, :create, :update, :destroy]
       end
-      resources :to_dos, only: [:create, :update]
+      resources :to_dos, only: [:create, :update, :destroy]
       get "analytics/week_overview", to: "analytics#week_overview"
     end
   end
