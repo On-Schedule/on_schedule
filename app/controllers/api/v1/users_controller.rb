57b07598a51daebd845be9e7a827aa1086544349
Doug Welchons <doug.welchons@gmail.com>
2024-12-04 11:01:32 -0700

    add new lins at EOF and minor refactors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,8 +13,13 @@ class Api::V1::UsersController < ApplicationController
   def project_users
     @users = User.find_by_sql(
       [
-        "SELECT users.id AS id, project_users.id AS project_user_id, users.first_name, users.last_name, project_users.user_level
-        FROM users LEFT OUTER JOIN project_users ON users.id = project_users.user_id AND project_users.project_id = ?",
+        "SELECT users.id AS id,
+        project_users.id AS project_user_id,
+        users.first_name, users.last_name,
+        project_users.user_level
+        FROM users LEFT OUTER JOIN project_users
+        ON users.id = project_users.user_id
+        AND project_users.project_id = ?",
         params[:project_id]
       ]
     )
